18626af2c80bafa9702d4dd396d93939fa3f2ed9
randymcmillan <randymcmillan@protonmail.com>
2026-09-05 17:23:10 -0400

    swiftyapp/swiftyapp/ContentView.swift:TerminalView

--- xcode/swiftyapp/swiftyapp/ContentView.swift
+++ xcode/swiftyapp/swiftyapp/ContentView.swift
@@ -10,19 +10,7 @@ import RustyLib
 
 struct ContentView: View {
     var body: some View {
-        print("ContentView: body property accessed.")
-        let helloMessage = rustHello()
-        let sum = rustAdd(a: 10, b: 32)
-        print("ContentView: rustHello() returned \(helloMessage)")
-        print("ContentView: rustAdd(a: 10, b: 32) returned \(sum)")
-        return VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(helloMessage)
-            Text(String(sum))
-        }
-        .padding()
+        TerminalView()
     }
 }
 
